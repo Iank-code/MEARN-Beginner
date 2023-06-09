c5343f9f203058acffc869d2514bd77716691a60
ianK-code <kian99564@gmail.com>
2023-06-09 10:43:16 +0300

    Refactored my routes

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -9,10 +9,29 @@ const getGoals = (req, res) => {
 // @desc Set goals
 // @route Post /api/goals
 // @access private
-const SetGoals = (req, res) => {
+const setGoal = (req, res) => {
   res.status(200).json({ message: "Get goals" });
+};
+
+
+// @desc Update goals
+// @route Put /api/goals/:id
+// @access private
+const updateGoal = (req, res) => {
+  res.status(200).json({ message: `Update goal ${req.params.id}` });
+};
+
+
+// @desc Delete goals
+// @route Delete /api/goals/:id
+// @access private
+const deleteGoal = (req, res) => {
+  res.status(200).json({ message: `Delete goal ${req.params.id}` });
 };
 
 module.exports = {
   getGoals,
+  setGoal,
+  updateGoal,
+  deleteGoal
 };
